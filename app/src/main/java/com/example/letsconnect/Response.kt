d77387d634465d435990efe37f2c63bbcc1d640e
Yashraj254 <yashr7651@gmail.com>
2022-11-02 18:55:28 +0530

    Integrated Google Sign in

---
+++ app/src/main/java/com/example/letsconnect/Response.kt
@@ -0,0 +1,14 @@
+package com.example.letsconnect
+
+
+sealed class Response<out T> {
+    object Loading: Response<Nothing>()
+
+    data class Success<out T>(
+        val data: T
+    ): Response<T>()
+
+    data class Failure(
+        val errorMessage: String
+    ): Response<Nothing>()
+}
---
+++ app/src/main/java/com/example/letsconnect/login/UserRegisterState.kt
@@ -0,0 +1,14 @@
+package com.example.letsconnect.login
+
+import androidx.activity.result.IntentSenderRequest
+import com.google.firebase.auth.FirebaseUser
+
+data class UserRegisterState(
+    val success : FirebaseUser? = null,
+    val error: String? = null
+)
+
+data class UserRegisterStateGoogle(
+    val success : IntentSenderRequest? = null,
+    val error: String? = null
+)
